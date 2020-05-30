40bafe2c346c9a4d942bd1e32cf1356403599abc
Lukáš Šandala <lukas.sandala@ixtent.com>
2020-05-30 19:26:29 +0200

    Added Checkout, membership and administration

---
+++ WingtipToys/WingtipToys/Checkout/CheckouktCancel.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="CheckouktCancel.aspx.cs" Inherits="WingtipToys.Checkout.CheckouktCancel" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
---
+++ WingtipToys/WingtipToys/Logic/PayPalBraintree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using WingtipToys;
+using WingtipToys.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Braintree;
+
+namespace WingtipToys.Logic
+{
+    public class ClientTokenHandler : IHttpHandler
+    {
+        BraintreeGateway gateway = new BraintreeGateway("access_token$sandbox$q9h79xnj9rn266mg$7d2b8f4cafad1b3f85f0de88bc554eea");
+
+        public bool IsReusable => true;
+
+        public void ProcessRequest(HttpContext context)
+        {
+            var clientToken = gateway.ClientToken.Generate();
+            context.Response.Write(clientToken);
+        }
+
+      
+    }
+
+    public class PayPalBraintree
+    {
+    }
+}
